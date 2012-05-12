7e09e6413305b666dd348ab5f938da902e37ea66
Irene <eirhnh@gmail.com>
2012-05-12 14:10:41 +0100

    refactor to separate pacman manager from game logic

---
+++ PacManGame.dart
@@ -0,0 +1,49 @@
+#library('PacMan');
+
+#import('dart:io');
+
+#import('Player.dart');
+#import('ConnectedPlayer.dart');
+#import('Message.dart');
+#import('Board.dart');
+#import('PacManServer.dart');
+
+class PacManGame {
+  Player pacman;
+  List<Player> players;
+  List<Player> ghosts;
+  Board board;
+  PacManGame(this.board) : players = [], ghosts = [] {
+    this.board.printBoard();
+  }
+  Player addPlayer(){
+    Player p = new Player(pacman == null, 0, 0);
+    if(pacman == null) {
+      pacman = p;
+    }
+    else {
+      ghosts.add(p);
+    }
+    players.add(p);
+    
+    return p;
+  }
+  
+  void setPlayerDirection(Player p, String direction) {
+    p.direction = direction;
+  }
+  
+  void handlePlayerDisconnect(Player p) {
+
+    //delete player from lists
+    int index = players.indexOf(p);
+    if(index !== -1) {
+      players.removeRange(index, 1);
+    }
+    index = ghosts.indexOf(p);
+    if(index !== -1) {
+      ghosts.removeRange(index, 1);
+    }
+  }
+  
+}
